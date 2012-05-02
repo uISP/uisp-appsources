fed508e0268f05617a358cf755b20ace3211c811
Andrew 'Necromant' Andrianov <necromant@necromant.ath.cx>
2012-05-02 18:29:52 +0400

    initial commit with 3 working apps

---
+++ cdc232.2011-06-24/mega48/default/dep/uart.o.d
@@ -0,0 +1,55 @@
+uart.o: ../uart.c \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/io.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/sfr_defs.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/inttypes.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/include/stdint.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/stdint.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/iom8.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/portpins.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/common.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/version.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/fuse.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/lock.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/interrupt.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/pgmspace.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/include/stddef.h \
+ ../../usbdrv/oddebug.h ../../usbdrv/usbportability.h \
+ ../../usbdrv/usbdrv.h ../usbconfig.h ../uart.h
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/io.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/sfr_defs.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/inttypes.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/include/stdint.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/stdint.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/iom8.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/portpins.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/common.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/version.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/fuse.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/lock.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/interrupt.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/pgmspace.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/include/stddef.h:
+
+../../usbdrv/oddebug.h:
+
+../../usbdrv/usbportability.h:
+
+../../usbdrv/usbdrv.h:
+
+../usbconfig.h:
+
+../uart.h:
---
+++ cdc232.2011-06-24/mega48/default/dep/usbdrv.o.d
@@ -0,0 +1,49 @@
+usbdrv.o: ../../usbdrv/usbdrv.c ../../usbdrv/usbportability.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/io.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/sfr_defs.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/inttypes.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/include/stdint.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/stdint.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/iom8.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/portpins.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/common.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/version.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/fuse.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/lock.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/pgmspace.h \
+ /opt/toolchains/avr/lib/gcc/avr/4.6.2/include/stddef.h \
+ ../../usbdrv/usbdrv.h ../usbconfig.h ../../usbdrv/oddebug.h
+
+../../usbdrv/usbportability.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/io.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/sfr_defs.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/inttypes.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/include/stdint.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/stdint.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/iom8.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/portpins.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/common.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/version.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/fuse.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/lock.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/../../../../avr/include/avr/pgmspace.h:
+
+/opt/toolchains/avr/lib/gcc/avr/4.6.2/include/stddef.h:
+
+../../usbdrv/usbdrv.h:
+
+../usbconfig.h:
+
+../../usbdrv/oddebug.h:
